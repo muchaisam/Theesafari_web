67435dea3ce79a7aabd31a6ab54c5b2daed96966
muchaisam <samson.muchai@strathmore.edu>
2023-04-04 08:49:27 +0300

    feat : hero redesign

--- src/partials/Hero.jsx
+++ src/partials/Hero.jsx
@@ -1,81 +1,81 @@
 import React, { useState } from 'react';
+
+import { FaPlaneDeparture, FaImages, FaTripadvisor } from 'react-icons/fa';
+
 function HeroHome() {
 
-  return (
-    <section className="relative">
-
-      <div class="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
-        <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
-          <div class="grid p-6 bg-gray-100 rounded place-content-center sm:p-8">
-            <div class="max-w-md mx-auto text-center lg:text-left">
-              <header>
-                <h2 class="text-xl font-bold text-gray-900 sm:text-3xl">Tembea Kenya</h2>
-
-                <p class="mt-4 text-gray-500">
-                Explore museums, galleries, and markets, and enjoy the diverse culinary scene. 
-                </p>
-              </header>
-
-              <a
-                href="#"
-                class="inline-block px-12 py-3 mt-8 text-sm font-medium text-white transition bg-gray-900 border border-gray-900 rounded hover:shadow focus:outline-none focus:ring"
-              >
-                Shop All
-              </a>
+    return (
+      <div className="mb-16">
+        <div className="bg-gray-100">
+          <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+             
+              <h2 className="max-w-lg mb-6 text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+                <span className="relative inline-block">
+                  <svg
+                    viewBox="0 0 52 24"
+                    fill="currentColor"
+                    className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-gray-400 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                  >
+                    <defs>
+                      <pattern
+                        id="dc223fcc-6d72-4ebc-b4ef-abe121034d6e"
+                        x="0"
+                        y="0"
+                        width=".135"
+                        height=".30"
+                      >
+                        <circle cx="1" cy="1" r=".7" />
+                      </pattern>
+                    </defs>
+                    <rect
+                      fill="url(#dc223fcc-6d72-4ebc-b4ef-abe121034d6e)"
+                      width="52"
+                      height="24"
+                    />
+                  </svg>
+                  <span className="relative">Tembea </span>
+                </span>{' '}
+                Kenya
+              </h2>
+              <p className="text-base text-gray-700 md:text-lg">
+              Discover the hidden gems of Kenya and embark on the adventure of a lifetime.
+              Immerse yourself in the vibrant culture and wildlife of this magnificent country, and make memories that will last a lifetime. Start your journey now and explore Kenya with us.
+              </p>
             </div>
           </div>
-
-          <div class="lg:col-span-2 lg:py-8">
-            <ul class="grid grid-cols-2 gap-4">
-              <li>
-                <a href="#" class="block group">
-                  <img
-                    src="https://images.unsplash.com/photo-1611348524140-53c9a25263d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmFpcm9iaXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60&auto=format&fit=crop&w=1598&q=80"
-                    alt="nbo"
-                    class="object-cover w-full rounded-2xl aspect-square"
-                  />
-
-                  <div class="mt-3">
-                    <h3
-                      class="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4"
-                    >
-                      Simple Watch
-                    </h3>
-
-                    <p class="mt-1 text-sm text-gray-700">$150</p>
-                  </div>
-                </a>
-              </li>
-
-              <li>
-                <a href="#" class="block group">
-                  <img
-                    src="https://images.unsplash.com/photo-1562082089-ae7d7acbd18d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5haXJvYml8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60&fit=crop&w=1598&q=80"
-                    alt="nbo"
-                    class="object-cover w-full rounded aspect-square"
-                  />
-
-                  <div class="mt-3">
-                    <h3
-                      class="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4"
-                    >
-                      Simple Watch
-                    </h3>
-
-                    <p class="mt-1 text-sm text-gray-700">$150</p>
-                  </div>
-                </a>
-              </li>
-            </ul>
+        </div>
+        <div className="relative px-4 sm:px-0">
+          <div className="absolute inset-0 bg-gray-100 h-1/2" />
+          <div className="relative grid mx-auto overflow-hidden bg-white divide-y rounded shadow sm:divide-y-0 sm:divide-x sm:max-w-screen-sm sm:grid-cols-3 lg:max-w-screen-md">
+            <div className="inline-block p-8 text-center">
+              <div className="flex items-center justify-center w-12 h-12 mx-auto mb-4 rounded-full bg-indigo-50">
+                <FaPlaneDeparture />
+              </div>
+              <p className="font-bold tracking-wide text-gray-800">
+                Editor's picks
+              </p>
+            </div>
+            <div className="inline-block p-8 text-center">
+              <div className="flex items-center justify-center w-12 h-12 mx-auto mb-4 rounded-full bg-indigo-50">
+                <FaImages />
+              </div>
+              <p className="font-bold tracking-wide text-gray-800">
+                Places Gallery
+              </p>
+            </div>
+            <div className="inline-block p-8 text-center">
+              <div className="flex items-center justify-center w-12 h-12 mx-auto mb-4 rounded-full bg-indigo-50">
+                <FaTripadvisor/>
+              </div>
+              <p className="font-bold tracking-wide text-gray-800">
+                Different Categories
+              </p>
+            </div>
           </div>
         </div>
       </div>
-
-
-
-
-    </section>
-  );
-}
+    );
+  };
 
 export default HeroHome;
