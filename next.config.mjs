351efebeb56ec225d0b9c47b0324db92731efb16
muchaisam <samson.muchai@strathmore.edu>
2024-05-23 11:48:26 +0300

    chore: Update next.config.mjs with remote image patterns and styled components support

---
+++ next.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com", // if your website has no www, drop it
+            },
+            {
+                protocol: "https",
+                hostname: "source.unsplash.com",
+            },
+            {
+                protocol: "https",
+                hostname: "i.ibb.co",
+            },
+            {
+                protocol: "https",
+                hostname: "pixabay.com",
+            },
+            {
+                protocol : "https",
+                hostname : "unsplash.com",
+            }
+        ],
+    },
+    compiler: {
+        styledComponents: true,
+    },
+};
+export default nextConfig;
