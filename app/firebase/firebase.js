08ce89ee69912c2acf305857ea6ce4247abd52bb
muchaisam <samson.muchai@strathmore.edu>
2024-06-04 12:49:26 +0300

    refactor: Update Firebase configuration to use environment variables

--- app/firebase/firebase.js
+++ app/firebase/firebase.js
@@ -1,19 +1,15 @@
-// Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
 import {getAnalytics} from "firebase/analytics";
 import {getFirestore} from "firebase/firestore";
 import {getAuth} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 let app;
 let analytics;
+/** @type {import("firebase/firestore").Firestore} */
+let db;
+let auth;
 
 if (typeof window !== 'undefined') {
-
     const firebaseConfig = {
         apiKey: process.env.FIREBASE_API_KEY,
         authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -23,18 +19,12 @@ if (typeof window !== 'undefined') {
         messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
         appId: process.env.FIREBASE_APP_ID,
         measurementId: process.env.FIREBASE_MEASUREMENT_ID
-
     };
 
-// Initialize Firebase
     app = initializeApp(firebaseConfig);
     analytics = getAnalytics(app);
+    db = getFirestore(app);
+    auth = getAuth(app);
 }
 
-// Initialize Firestore
-const db = getFirestore(app);
-
-// Initialize Firebase Auth
-const auth = getAuth(app);
-
-export {db, auth};
+export {app, analytics, db, auth};
